7be2dda7c097e9124062fec23f9fdcbf105faacc
ujjwal <ujjwal01kumar@gmail.com>
2017-05-02 23:28:44 +0530

    Welcome message

--- kilo.c
+++ kilo.c
@@ -10,6 +10,7 @@
 
 // defines
 
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 // data
@@ -101,7 +102,7 @@ struct abuf{
 	int len;
 };
 
-#define ABUF_INIT {NULL,0};
+#define ABUF_INIT {NULL, 0};
 
 void abAppend(struct abuf *ab,const char *s,int len){
 	char *new = realloc(ab->b,ab->len + len);
@@ -119,11 +120,25 @@ void abFree(struct abuf *ab){
 // output
 
 void editorDrawRows(struct abuf *ab){
-	int t;
-	for(t = 0;t < E.screenrows; t++){
-		abAppend(ab, "~", 1);
+	int y;
+	for(y = 0;y < E.screenrows; y++){
+		if (y == E.screenrows / 3) {
+      		char welcome[80];
+      		int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+      		if (welcomelen > E.screencols) welcomelen = E.screencols;
+      		abAppend(ab, welcome, welcomelen);
+      		int padding = (E.screencols - welcomelen) / 2;
+      		if(padding){
+      			abAppend(ab, "~", 1);
+      			padding--;
+      		}
+      		while(padding--) abAppend(ab, " ", 1);
+    	} else {
+      		abAppend(ab, "~", 1);
+    	}
 
-		if(t < E.screenrows - 1){
+		abAppend(ab, "\x1b[K", 3);
+		if(y < E.screenrows - 1){
 			abAppend(ab, "\r\n", 2);
 		}
 	}
@@ -132,15 +147,16 @@ void editorDrawRows(struct abuf *ab){
 void editorRefreshScreen(){
 	struct abuf ab = ABUF_INIT;
 
-	abAppend(&ab, "\x1b[2J", 4);
+	abAppend(&ab, "\x1b[?25l", 6);
 	abAppend(&ab, "\x1b[H", 3);
 
 	editorDrawRows(&ab);
 
 	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25l", 6);
 
 	write(STDOUT_FILENO, ab.b, ab.len);
-	abFree(%ab);
+	abFree(&ab);
 }
 
 // input
@@ -163,6 +179,8 @@ void initEditor(){
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1) 
 		die("getWindowSize");
 }
+
+// driver function
 
 int main(){
 	enableRawMode();
