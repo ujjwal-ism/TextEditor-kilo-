62777e11285fbd0e70e6be937be64a4b3ba28749
ujjwal <ujjwal01kumar@gmail.com>
2017-05-06 20:14:35 +0530

    Input handling through files(modified)

--- kilo.c
+++ kilo.c
@@ -168,11 +168,14 @@ int getWindowSize(int *rows,int *cols){
 // row operations
 
 void editorAppendRow(char *s, int len){
-	E.row.size = len;
-	E.row.chars = malloc(len + 1);
-	memcpy(E.row.chars, s, len);
-	E.row.chars[len] = '\0';
-	E.numrows = 1;
+	E.row = realloc(E.row, sizeof(erow)*(E.numrows + 1));
+
+	int at = E.numrows;
+	E.row[at].size = len;
+	E.row[at].chars = malloc(len + 1);
+	memcpy(E.row[at].chars, s, len);
+	E.row[at].chars[len] = '\0';
+	E.numrows++;
 }
 
 // file input/output
@@ -184,8 +187,7 @@ void editorOpen(char *filename){
 	char *line = NULL;
 	size_t linecap = 0;
 	ssize_t linelen;
-	linelen = getline(&line, &linecap, fp);
-	if(linelen != -1){
+	while((linelen = getline(&line, &linecap, fp)) != -1){
 		while(linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
 			linelen--;
 		editorAppendRow(line, linelen);
@@ -237,9 +239,9 @@ void editorDrawRows(struct abuf *ab){
       		abAppend(ab, "~", 1);
     		}
     	}else{
-    		int len = E.row.size;
+    		int len = E.row[y].size;
     		if(len > E.screencols) len = E.screencols;
-    		abAppend(ab, E.row.chars, len);
+    		abAppend(ab, E.row[y].chars, len);
     	}
 
 		abAppend(ab, "\x1b[K", 3);
