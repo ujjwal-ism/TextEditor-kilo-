1178a1b95ab7e5e16c072c3704988165e70eab35
ujjwal <ujjwal01kumar@gmail.com>
2017-05-07 00:38:11 +0530

    Scrolling(modified)

--- kilo.c
+++ kilo.c
@@ -281,7 +281,7 @@ void editorRefreshScreen(){
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.cx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 
 	abAppend(&ab, "\x1b[?25h", 6);
@@ -293,24 +293,43 @@ void editorRefreshScreen(){
 // input
 
 void editorMoveCursor(int key){
+	erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
 	switch(key){
 		case ARROW_UP:
 			if(E.cy != 0)
 				E.cy--;
 			break;
+
 		case ARROW_LEFT:
 			if(E.cx != 0)
 				E.cx--;
+			else if (E.cy > 0){
+				E.cy--;
+				E.cx = E.row[E.cy].size;
+			}
 			break;
+
 		case ARROW_DOWN:
 			if(E.cy < E.numrows)
 				E.cy++;
 			break;
+
 		case ARROW_RIGHT:
-			if(E.cx != E.screencols - 1)
+			if(row && E.cx < row->size)
 				E.cx++;
+			else if(row && E.cx == row->size){
+				E.cy++;
+				E.cx = 0;
+			}
 			break;
 	}
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  	int rowlen = row ? row->size : 0;
+  	if (E.cx > rowlen) {
+    	E.cx = rowlen;
+  	}
 }
 
 void editorProcessKeypress(){
