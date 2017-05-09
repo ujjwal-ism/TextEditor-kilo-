666e8672106cb7a08a52066dd7f562abfc83e1dc
ujjwal <ujjwal01kumar@gmail.com>
2017-05-10 00:10:10 +0530

    Saving file to disk

--- kilo.c
+++ kilo.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,6 +25,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey{
+	BACKSPACE = 127,
 	ARROW_LEFT = 1000,
 	ARROW_RIGHT ,
 	ARROW_UP ,
@@ -60,6 +62,10 @@ struct editorConfig {
 };
 
 struct editorConfig E;
+
+// prototypes
+
+void editorSetStatusMessage(const char *fmt, ...);
 
 // terminal
 
@@ -252,6 +258,25 @@ void editorInsertChar(int c){
 
 // file input/output
 
+char *editorRowsToString(int *buflen){
+	int totlen = 0;
+	int j;
+	for(j = 0;j < E.numrows; j++){
+		totlen += E.row[j].size + 1;
+	}
+	*buflen = totlen;
+
+	char *buf = malloc(totlen);
+	char *p = buf;
+	for(j = 0; j < E.numrows; j++){
+		memcpy(p, E.row[j].chars, E.row[j].size);
+		p += E.row[j].size;
+		*p = '\n';
+		p++;
+	}
+	return buf;
+}
+
 void editorOpen(char *filename){
 	free(E.filename);
 	E.filename = strdup(filename);
@@ -269,6 +294,28 @@ void editorOpen(char *filename){
 	}
 	free(line);
 	fclose(fp);
+}
+
+void editorSave(){
+	if(E.filename == NULL) return;
+
+	int len;
+	char *buf = editorRowsToString(&len);
+
+	int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+	if(fd != -1){
+  		if(ftruncate(fd, len) != -1){
+  			if(write(fd, buf, len) == len){
+  				close(fd);
+  				free(buf);
+  				editorSetStatusMessage("%d bytes written to disk", len);
+  				return;
+  			}
+  		}
+  		close(fd);
+  	}
+  	free(buf);
+  	editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 // append buffer
@@ -451,10 +498,18 @@ void editorProcessKeypress(){
 	int c = editorReadKey();
 
 	switch(c){
-		case CTRL_KEY('q'):
+		case '\r' :
+			// TODO
+			break;
+
+		case CTRL_KEY('q') :
 			write(STDOUT_FILENO ,"\x1b[2J", 4);
 			write(STDOUT_FILENO,"\x1b[H", 3);
 			exit(0);
+			break;
+
+		case CTRL_KEY('s') : 
+			editorSave();
 			break;
 
 		case HOME_KEY :
@@ -463,6 +518,12 @@ void editorProcessKeypress(){
 		case END_KEY : 
 			if(E.cy < E.numrows)
 				E.cx = E.row[E.cy].size;
+			break;
+
+		case BACKSPACE :
+		case CTRL_KEY('h') :
+		case DEL_KEY :
+			// TODO
 			break;
 
 		case PAGE_UP :
@@ -486,6 +547,10 @@ void editorProcessKeypress(){
 		case ARROW_DOWN :
 		case ARROW_RIGHT :
 			editorMoveCursor(c);
+			break;
+
+		case CTRL_KEY('l') :
+		case '\x1b' :
 			break;
 
 		default :
@@ -522,7 +587,7 @@ int main(int argc, char *argv[]){
 		editorOpen(argv[1]);
 	}
 
-	editorSetStatusMessage("HELP: Ctrl-Q = quit");
+	editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
 	while(1){
 		editorRefreshScreen();
