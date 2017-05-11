bfe59fb8f0b9d46ffb38d03b0899b21b71d2a774
ujjwal <ujjwal01kumar@gmail.com>
2017-05-11 19:11:40 +0530

    Enter Key added

--- kilo.c
+++ kilo.c
@@ -222,10 +222,12 @@ void editorUpdateRow(erow *row){
 	row->rsize = idx;
 }
 
-void editorAppendRow(char *s, int len){
-	E.row = realloc(E.row, sizeof(erow)*(E.numrows + 1));
+void editorInsertRow(int at, char *s, size_t len){
+	if(at < 0 || at > E.numrows) return;
 
-	int at = E.numrows;
+	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+	memmove(&E.row[at + 1], &E.row[at], sizeof(erow) * (E.numrows - at));
+
 	E.row[at].size = len;
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
@@ -283,10 +285,26 @@ void editorRowDelChar(erow *row, int at){
 
 void editorInsertChar(int c){
 	if(E.cy == E.numrows){
-		editorAppendRow("", 0);
+		editorInsertRow(E.numrows, "", 0);
 	}
 	editorRowInsertChar(&E.row[E.cy], E.cx, c);
 	E.cx++;
+}
+
+void editorInsertNewLine(){
+	if(E.cx == 0){
+		editorInsertRow(E.cy, "", 0);
+	}
+	else{
+		erow *row = &E.row[E.cy];
+		editorInsertRow(E.cy + 1, &row->chars[E.cx], row->size - E.cx);
+		row = &E.row[E.cy];
+		row->size = E.cx;
+		row->chars[row->size] = '\0';
+		editorUpdateRow(row);
+	}
+	E.cy++;
+	E.cx = 0;
 }
 
 void editorDelChar(){
@@ -339,7 +357,7 @@ void editorOpen(char *filename){
 	while((linelen = getline(&line, &linecap, fp)) != -1){
 		while(linelen > 0 && (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
 			linelen--;
-		editorAppendRow(line, linelen);
+		editorInsertRow(E.numrows, line, linelen);
 	}
 	free(line);
 	fclose(fp);
@@ -552,7 +570,7 @@ void editorProcessKeypress(){
 
 	switch(c){
 		case '\r' :
-			// TODO
+			editorInsertNewLine();
 			break;
 
 		case CTRL_KEY('q') :
