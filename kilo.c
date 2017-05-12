e180173f342ca9b75af8c5c5a54e9c562dd7a55d
ujjwal <ujjwal01kumar@gmail.com>
2017-05-12 22:47:06 +0530

    Incremental search implemented

--- kilo.c
+++ kilo.c
@@ -69,7 +69,7 @@ struct editorConfig E;
 
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 // terminal
 
@@ -198,6 +198,19 @@ int editorRowCxToRx(erow *row, int cx){
 		rx++;
 	}
 	return rx;
+}
+
+int editorRowRxToCx(erow *row, int rx){
+	int cur_rx = 0;
+	int cx;
+	for(cx = 0; cx < row->size; cx++){
+		if(row->chars[cx] == '\t')
+			cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+		cur_rx++;
+
+		if(cur_rx > rx) return cx;
+	}
+	return cx;
 }
 
 void editorUpdateRow(erow *row){
@@ -368,7 +381,7 @@ void editorOpen(char *filename){
 
 void editorSave(){
 	if(E.filename == NULL) {
-		E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+		E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
 		if(E.filename == NULL){
 			editorSetStatusMessage("Save aborted");
 			return;
@@ -393,6 +406,34 @@ void editorSave(){
   	}
   	free(buf);
   	editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+// find
+
+void editorFindCallback(char *query, int key){
+	if(key == '\r' || key == '\x1b'){
+		return;
+	}
+
+	int i;
+	for(i = 0; i < E.numrows; i++){
+		erow *row = &E.row[i];
+		char *match = strstr(row->render, query);
+		if (match) {
+      		E.cy = i;
+      		E.cx = editorRowRxToCx(row, match - row->render);
+      		E.rowoff = E.numrows;
+      		break;
+    	}
+	}
+}
+
+void editorFind(){
+	char *query = editorPrompt("Search %s (ESC to cancel)", editorFindCallback);
+	
+	if(query){
+		free(query);
+	}
 }
 
 // append buffer
@@ -446,7 +487,7 @@ void editorDrawRows(struct abuf *ab){
 		if(filerow >= E.numrows){
 			if (E.numrows == 0 &&  y == E.screenrows / 3) {
       			char welcome[80];
-      			int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+      			int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- Ujjwal's version %s", KILO_VERSION);
       			if (welcomelen > E.screencols) welcomelen = E.screencols;
       			abAppend(ab, welcome, welcomelen);
       			int padding = (E.screencols - welcomelen) / 2;
@@ -531,7 +572,7 @@ void editorSetStatusMessage(const char *fmt, ...){
 
 // input
 
-char *editorPrompt(char *prompt){
+char *editorPrompt(char *prompt, void (*callback)(char *, int)){
 	size_t bufsize = 128;
 	char *buf = malloc(bufsize);
 
@@ -548,12 +589,14 @@ char *editorPrompt(char *prompt){
 		}
 		else if(c == '\x1b'){
 			editorSetStatusMessage("");
+			if (callback) callback(buf , c);
 			free(buf);
 			return NULL;
 		}
 		else if(c == '\r'){
 			if(buflen != 0){
 				editorSetStatusMessage("");
+				if (callback) callback(buf , c);
 				return buf;
 			}
 		}
@@ -565,6 +608,8 @@ char *editorPrompt(char *prompt){
 			buf[buflen++] = c;
 			buf[buflen] = '\0';
 		}
+
+		if (callback) callback(buf , c);
 	}
 }
 
@@ -641,6 +686,10 @@ void editorProcessKeypress(){
 				E.cx = E.row[E.cy].size;
 			break;
 
+		case CTRL_KEY('f'):
+      		editorFind();
+      		break;
+
 		case BACKSPACE :
 		case CTRL_KEY('h') :
 		case DEL_KEY :
@@ -712,7 +761,7 @@ int main(int argc, char *argv[]){
 		editorOpen(argv[1]);
 	}
 
-	editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+	editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
 	while(1){
 		editorRefreshScreen();
